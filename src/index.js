392a4bf347f52560dc8be31761a1ad11b6b46b26
Anna Zankevich <annzankevich18@gmail.com>
2019-03-21 00:11:05 +0300

    add:check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    let array = str.split('');
+    let brackets = bracketsConfig;
+    let i = 0;
+    while (i < array.length) {
+        let isBroke = false;
+        for (let j = 0; j < brackets.length; j++) {
+            if (array[i] === brackets[j][0] && array[i + 1] === brackets[j][1]) {
+                array.splice(i, 2);
+                if (array.length === 0) {
+                    return true;
+                }
+                i = 0;
+                isBroke = true;
+                break;
+            }
+        }
+        if (isBroke) {
+            continue;
+        }
+
+        i++;
+    }
+    return false;
 }
